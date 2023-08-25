790e44d0d38da76560d6431a71434562cfc74d81
Paksi Mega Bumi <paksimegabumi@gmail.com>
2023-08-26 00:43:43 +0700

    Added test for customer not found expected to throw exception

--- src/test/java/com/paksi/tokopediaassignment/customer/CustomerServiceTests.java
+++ src/test/java/com/paksi/tokopediaassignment/customer/CustomerServiceTests.java
@@ -1,9 +1,40 @@
 package com.paksi.tokopediaassignment.customer;
 
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class CustomerServiceTests {
-    
+class CustomerServiceTests {
+    @InjectMocks
+    private CustomerService customerService;
+
+    @Mock
+    private CustomerRepository customerRepository;
+
+    @Test
+    void findById_shouldCallCustomerRepositoryFindByIdOnce_whenGivenIdIs1() {
+        Customer customer = Customer.builder().id(1L).build();
+        Optional<Customer> existingOptionalCustomer = Optional.of(customer);
+        Mockito.when(this.customerRepository.findById(customer.getId())).thenReturn(existingOptionalCustomer);
+
+        this.customerService.findById(customer.getId());
+
+        Mockito.verify(this.customerRepository, Mockito.times(1)).findById(customer.getId());
+    }
+
+    @Test
+    void findById_shouldThrowCustomerNotFoundException_whenGivenIdIs1AndCustomerDoesNotExistsInDatabase() {
+        Customer customer = Customer.builder().id(1L).build();
+        Optional<Customer> existingOptionalCustomer = Optional.empty();
+        Mockito.when(this.customerRepository.findById(customer.getId())).thenReturn(existingOptionalCustomer);
+        
+        Assertions.assertThrows(CustomerNotFoundException.class, () -> this.customerService.findById(customer.getId()));
+    }
 }
