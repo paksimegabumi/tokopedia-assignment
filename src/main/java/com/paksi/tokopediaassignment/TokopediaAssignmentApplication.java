9112bd091a5709eef62406bdcc1831c83b0ad7dc
Paksi Mega Bumi <paksimegabumi@gmail.com>
2023-08-25 12:11:45 +0700

    Initial Commit

---
+++ src/main/java/com/paksi/tokopediaassignment/TokopediaAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.paksi.tokopediaassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokopediaAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokopediaAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paksi/tokopediaassignment/TokopediaAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paksi.tokopediaassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokopediaAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
