ead7e7a4d9f1f00f5ee1824ccaf542494d1dea18
Paksi Mega Bumi <paksimegabumi@gmail.com>
2023-08-29 14:45:26 +0700

    remove unused imports

--- src/main/java/com/paksi/tokopediaassignment/payment/model/dto/PaymentRequestDTO.java
+++ src/main/java/com/paksi/tokopediaassignment/payment/model/dto/PaymentRequestDTO.java
@@ -7,8 +7,6 @@ import com.paksi.tokopediaassignment.payment.model.Payment;
 import com.paksi.tokopediaassignment.paymenttype.PaymentType;
 
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
