bc4c18c5680b8073f35ecac610b99af779dd3312
Paksi Mega Bumi <paksimegabumi@gmail.com>
2023-08-31 17:20:37 +0700

    Remove unused import

--- src/main/java/com/paksi/tokopediaassignment/payment/PaymentController.java
+++ src/main/java/com/paksi/tokopediaassignment/payment/PaymentController.java
@@ -1,14 +1,12 @@
 package com.paksi.tokopediaassignment.payment;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;Ø
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
